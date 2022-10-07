105f30e0d9a3b1d2a02020a73e91617b2b7d6884
carlinjshaw <carlinjshaw@gmail.com>
2022-10-07 15:19:55 -0400

    building port

---
+++ README.md
@@ -0,0 +1,5 @@
+# Portfolio
+This portfolio is a React Application that showcases some of my Web Development work. 
+
+# Link
+https://carlinjshaw.github.io/portfolio-react/
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+
+// function Footer(props) {
+//     return (
+//         <div>
+//             <a href="https://github.com/carlinjshaw">Github</a>
+
+            
+//             <a href="https://www.linkedin.com/in/carlin-shaw-352b8bb1/">LinkedIn</a>
+//         </div>
+//     );
+// }
+
+// export default Footer;
